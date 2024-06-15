85155093f1fbc16bc8180dc20ed5ee1b206f5971
Andrey <zizkun.av@list.ru>
2024-06-15 18:53:28 +0400

    Commit

---
+++ src/test/java/tests/ManualTests.java
@@ -0,0 +1,18 @@
+package tests;
+
+import io.qameta.allure.Manual;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
+
+public class ManualTests {
+    @Test
+    @Manual
+    @DisplayName("Мануальный тест - Наличие текста у чекбокса 'Подписаться на рассылку'")
+    public void unsuccessfulAuth() {
+        step("Открыть стартовую страницу сайта https://www.aviasales.ru");
+        step("Переход на страницу 'Профиль'");
+        step("Проверка наличия текста 'Подписаться на рассылку'");
+    }
+}
